aa6076be8ba24ba6cea16591cb32480e527e57f0
Allan Kildare <allankildare27@gmail.com>
2021-03-11 02:32:41 -0300

    + atributo title nas imagens

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-let botao = document.getElementById("botao")
-let botaoHoje = document.getElementById("hoje")
-let key = 'laQUTbkbRZHvE99chEF5U8VBcDOnqgHcBH2zlnDI'
+const botao = document.getElementById("botao")
+const botaoHoje = document.getElementById("hoje")
+const key = 'laQUTbkbRZHvE99chEF5U8VBcDOnqgHcBH2zlnDI'
 
-let container = document.getElementById("imagem-apod")
+let imgContainer = document.getElementById("imagem-apod")
 
-let req = new XMLHttpRequest()
+const req = new XMLHttpRequest()
 
 // carregando foto de hoje da APOD 
 req.open('GET', `https://api.nasa.gov/planetary/apod?api_key=${key}`, false)
 req.addEventListener("load", function () {
-    if ( this.status === 200 && this.readyState === 4) {
+    if ( this.status === 200 && this.readyState === 4 ) {
         let dados = JSON.parse(req.responseText)
         let img = dados.url
 
-        container.innerHTML += `<img src="${img}" class="img-apod" id="imagem">`
+        imgContainer.innerHTML += `<img src="${img}" title="${dados.title}" class="img-apod" id="imagem">`
     }
 })
 req.send()
 
-let newReq = new XMLHttpRequest()
+const newReq = new XMLHttpRequest()
 
 // botao para data especifica
 botao.addEventListener("click", function () {
     let date = document.getElementById("data").value
     
     newReq.onreadystatechange = function () {
-        if ( this.status === 200 && this.readyState === 4) {
+        if ( this.status === 200 && this.readyState === 4 ) {
             let dados = JSON.parse(newReq.responseText)
             let img = dados.url
             imagem.src = img
+            imagem.title = dados.title
         }
     }
     newReq.open('GET', `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`, false)
